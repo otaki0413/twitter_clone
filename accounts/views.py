d7937144a3bc54af4dc7832cb831680c4f8ee320
otaki0413 <takida0413@gmail.com>
2024-11-03 16:49:13 +0900

    ✨ メッセージ表示の設定

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.base import TemplateView
+from django.contrib import messages
+
 
 from allauth.account.views import SignupView, LoginView
 
@@ -11,6 +13,9 @@ class CustomSignupView(SignupView):
     def form_valid(self, form):
         # 親のform_valid()を呼ぶことで、内部的にユーザー登録からリダイレクトまで行う
         response = super().form_valid(form)
+        messages.success(
+            self.request, "ユーザー登録に成功しました。", extra_tags="success"
+        )
         return response
 
     def get_success_url(self):
@@ -20,11 +25,15 @@ class CustomSignupView(SignupView):
 class CustomLoginView(LoginView):
     """カスタムログインビュー"""
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
     def get_success_url(self):
         return reverse_lazy("accounts:home")
 
 
-class HogeView(TemplateView):
+class HomeView(TemplateView):
     template_name = "account/home.html"
 
 
