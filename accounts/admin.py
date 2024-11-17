741e5ee1449865b4fe73735a381360d35b57ae2f
otaki0413 <takida0413@gmail.com>
2024-11-17 16:38:26 +0900

    ✨ フォロー関係のモデルの管理サイト登録

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from .models import FollowRelation
 
 CustomUser = get_user_model()
 
 admin.site.register(CustomUser)
+
+
+@admin.register(FollowRelation)
+class FollowRelationAdmin(admin.ModelAdmin):
+    model = FollowRelation
+    readonly_fields = ("created_at", "updated_at")
