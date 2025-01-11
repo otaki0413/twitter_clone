d0ac399d214c3c32f500920d83530e5f6b814542
otaki0413 <takida0413@gmail.com>
2025-01-11 08:07:10 +0000

    ✨ メッセージモデルとマイグレーション作成

--- direct_messages/models.py
+++ direct_messages/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class AbstractCommon(models.Model):
+    """共通フィールド用の抽象基底クラス"""
+
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField("登録日時", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日時", auto_now=True)
+
+
+class Message(AbstractCommon):
+    """メッセージ情報の格納用モデル"""
+
+    class Meta:
+        db_table = "message"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["sender", "receiver"],
+                name="unique_message_relation",
+            )
+        ]
+
+    sender = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    receiver = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="received_messages"
+    )
+    content = models.TextField("メッセージ内容", null=False, blank=False)
