c89bb0872d181115b07d7f741b1bbf8093c80dca
otaki0413 <takida0413@gmail.com>
2025-01-11 05:07:21 +0000

    ✨ メッセージ一覧ビューとメッセージ部屋ビューのリファクタリング

--- direct_messages/views.py
+++ direct_messages/views.py
@@ -1,16 +1,27 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from accounts.models import CustomUser
 
-class MessageListView(TemplateView):
+
+class MessageListView(LoginRequiredMixin, ListView):
     """メッセージ一覧ビュー"""
 
-    template_name = "messages/index.html"
+    template_name = "direct_messages/index.html"
+    context_object_name = "followers"
+
+    def get_queryset(self):
+        # ログインユーザ取得
+        user = self.request.user
+        # ログインユーザのフォロワー取得
+        return CustomUser.objects.filter(following_relations__followee=user)
 
 
-class MessageRoomView(TemplateView):
+class MessageRoomView(LoginRequiredMixin, DetailView):
     """メッセージ部屋ビュー"""
 
+    model = CustomUser
     slug_field = "username"
     slug_url_kwarg = "username"
-    template_name = "messages/message_room.html"
+    template_name = "direct_messages/message_room.html"
+    context_object_name = "follower"
