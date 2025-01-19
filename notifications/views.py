9a13f23d3d05fd68aa3366756f559adaf20c2993
otaki0413 <takida0413@gmail.com>
2025-01-19 16:33:43 +0900

    ✨ 通知情報の取得処理の実装

--- notifications/views.py
+++ notifications/views.py
@@ -9,3 +9,13 @@ class NotificationListView(LoginRequiredMixin, ListView):
 
     model = Notification
     template_name = "notifications/index.html"
+    context_object_name = "notification_list"
+
+    def get_queryset(self):
+        # ログインユーザーへの通知情報を返す
+        user = self.request.user
+        return user.get_notifications()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        return context
